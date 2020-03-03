a5462d5a19022b5004164bf995a550398c2350be
DiegoSalazar27 <diegosalazargm@gmail.com>
2020-03-03 18:14:33 -0400

    Adding functionality

--- master.js
+++ master.js
@@ -1,10 +1,17 @@
 const rules__button = document.getElementById('rules__button')
 const rulesContainer = document.getElementById('rulesContainer')
+const rulesImg = document.getElementById('rulesImg')
 const closeRulesContainer = document.getElementById('closeRulesContainer')
 
 const scoreTag = document.getElementById('score')
 const main = document.getElementById('main')
 const results = document.getElementById('results')
+const user = results.firstElementChild.lastElementChild
+const house = results.lastElementChild.lastElementChild
+const conclusion__title = document.getElementById('conclusion__title')
+const play_again = document.getElementById('play_again')
+
+const rulesBonus = './images/image-rules-bonus.svg'
 var user__response = false
 var score = 0
 
@@ -35,6 +42,8 @@ const spock = {
 
 if (user__response) {
   options.push(lizzard, spock)
+  rulesImg.setAttribute('src', rulesBonus)
+  main.classList.add('bonus')
 }
 
 const options__buttons = options.map(element => createElement(element))  //Creating button elements for the game
@@ -48,21 +57,38 @@ window.addEventListener('load', () =>{
   options__buttons.forEach(element => {
     element.addEventListener('click', game)
   })
+
+  play_again.addEventListener('click', () =>{
+    options__buttons.forEach(element => {
+      element.addEventListener('click', game)
+    })
+    results.classList.remove('results--show')
+    main.classList.remove('mainHide')
+  })
 })
 
-function createElement(element){
-  let container = document.createElement('div')
-  let img = document.createElement('img')
+function createElement(element, containerDiv, Containerimg ){
+  let container = containerDiv
+  let img = Containerimg
+  let created = true
+
+  if (!container) {
+    container = document.createElement('div')
+    img = document.createElement('img')
+    created = false
+  }
 
   container.setAttribute('class', 'main__button')
-  container.setAttribute('id', element.name)
+  container.classList.add(element.name)
   container.dataset.type = element.name;
 
   img.setAttribute('class', 'main__button__option')
   img.setAttribute('src', `./images/icon-${element.name}.svg`)
   img.setAttribute('alt', element.name)
 
-  container.appendChild(img)
+  if (!created) {
+    container.appendChild(img)
+  }
 
   return container
 }
@@ -88,33 +114,35 @@ function startGame(user_selection){
   let rand = Math.floor(Math.random() * (options.length - 1))  
   const house_selection = options[rand]
 
-  let result = createElement(house_selection)
-  results.lastElementChild.appendChild(result)
-  result = createElement(user_selection[0])
-  results.firstElementChild.appendChild(result)
+  createElement(house_selection, house, house.firstElementChild)
+  createElement(user_selection[0], user, user.firstElementChild)
 
   results.classList.add('results--show')
 
   if (house_selection.name === user_selection[0].name) {
     tieGame()
-  }else if(house_selection.name === user_selection[0].kill[0] || house_selection.name === user_selection[0].kill[0]){
+  }else if(house_selection.name === user_selection[0].kill[0] || house_selection.name === user_selection[0].kill[1]){
     userWins()
   }else{
     userLoses()
   }
   scoreTag.innerHTML = score
+  setTimeout(() =>{
+    house.parentElement.style.setProperty('transform', 'translate(0)')
+    user.parentElement.style.setProperty('transform', 'translate(0)')
+  }, 1000)
 }
 
 function tieGame(){
-  console.log('Tie')
+  conclusion__title.innerHTML = 'Tie Game' 
 }
 
 function userWins(){
+  conclusion__title.innerHTML = 'You win'
   score++
-  console.log('Wins')
 }
 
 function userLoses(){
+  conclusion__title.innerHTML = 'You lose'
   score > 0 ? score-- : null
-  console.log('Loses')
 }
